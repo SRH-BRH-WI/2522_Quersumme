24be3ecae2807aad90c8453f1700e9bc65e8e38e
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-17 15:48:50 +0100

    Aufgabenstellung ergänzt

--- src/Quersumme.java
+++ src/Quersumme.java
@@ -1,2 +1,9 @@
 public class Quersumme {
+
+    // Eingabe: Bitte eine max. 5 stellige Zahl eingeben: 1234
+    // Ausgabe: 4 + 3 + 2 + 1 = 10
+
+    public static void main(String[] args) {
+
+    }
 }
